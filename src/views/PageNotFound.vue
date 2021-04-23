1729cc4f97fa8284384d282b7fd8746ee691f74a
flaaaps <julianwening4@gmail.com>
2021-04-23 19:36:59 +0200

    Create 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Not the page you are looking for</h1>
+        <router-link to="/">Go home</router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: "PageNotFund",
+}
+</script>
