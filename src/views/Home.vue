9dda13eb2ef28bf28c22605df42e245268c3ca65
flaaaps <julianwening4@gmail.com>
2021-04-23 22:10:29 +0200

    Update home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="home">
-        <Clock msg="Welcome to Your Vue.js App" />
+        <div
+            :key="player.id"
+            v-for="player in players"
+            @click="changePlayer"
+            :class="['player-' + player.id.toString(), activePlayer === player.id && 'active']"
+        >
+            <Clock
+                @game-over="handleGameOver"
+                :running="running && activePlayer === player.id"
+                :duration="10"
+                :name="player.name"
+            />
+        </div>
+        <br />
+        <button @click="start" v-if="!running">Start</button>
     </div>
 </template>
 
@@ -13,5 +27,46 @@ export default {
     components: {
         Clock,
     },
+    methods: {
+        start: function () {
+            this.running = true
+            this.started = true
+        },
+        changePlayer: function () {
+            console.log(this.started)
+            if (this.running || !this.started)
+                this.activePlayer === 1 ? (this.activePlayer = 2) : (this.activePlayer = 1)
+            console.log("CHANGED")
+        },
+        updateClock: function () {
+            if (!this.started) {
+                this.started = true
+            } else {
+                this.activePlayer === 1 ? (this.activePlayer = 2) : (this.activePlayer = 1)
+            }
+        },
+        handleGameOver: function () {
+            console.log("GAME IS OVER!!")
+            this.running = false
+        },
+    },
+    data() {
+        return {
+            started: false,
+            running: false,
+            text: "hello",
+            activePlayer: 1,
+            players: [
+                { name: "Detlef", id: 1 },
+                { name: "Joshi", id: 2 },
+            ],
+        }
+    },
 }
 </script>
+
+<style>
+.active {
+    color: green;
+}
+</style>
